ca085f9a6a59f854830c902017897abb577da497
mentionyou <694355669@qq.com>
2012-06-28 22:03:44 +0800

    Update master

--- recover_0.1.c
+++ recover_0.1.c
@@ -1,10 +1,3 @@
-/*
-* /extcarve/extcarve.c-ext2/ext3/ext4 Undelete Tool.
-*
-* Copyright (C) 2007,2008,2009,2010,2011 Lakshmipathi.G <lakshmipathi.g@gmail.com>
-* Visit www.giis.co.in for manuals or docs.
-*/
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -16,9 +9,9 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <errno.h>
-#include <assert.h>		/* assert */
+#include <assert.h>		
 #include <libgen.h>
-/* argp */
+
 #include <argp.h>
 
 
@@ -36,7 +29,6 @@ struct arguments
 
 struct extcarve_meta
 {
-//indicates whether header/footer is found.
   int header_found, footer_found;
   unsigned long header_blk;
   unsigned long footer_blk;
@@ -161,14 +153,6 @@ main (int argc, char *argv[])
   return 1;
 }
 
-
-/*
- * unused.c --- quick and dirty unused space dumper
- *
- * Copyright (C) 1997 Theodore Ts'o.  This file may be redistributed
- * under the terms of the GNU Public License.
- */
-//void do_dump_unused(int argc EXT2FS_ATTR((unused)), char **argv)
 void
 do_dump_unused (ext2_filsys current_fs)
 {
@@ -177,11 +161,6 @@ do_dump_unused (ext2_filsys current_fs)
   unsigned int i;
   errcode_t retval, f_retval;
   struct extcarve_meta needle = { 0 };
-
-/*	if (common_args_process(argc, argv, 1, 1,
-				"dump_unused", "", 0))
-		return;*/
-
   for (blk = current_fs->super->s_first_data_block;
        blk < current_fs->super->s_blocks_count; blk++)
     {
@@ -190,25 +169,18 @@ do_dump_unused (ext2_filsys current_fs)
       retval = io_channel_read_blk (current_fs->io, blk, 1, buf);
       if (retval)
 	{
-	  //      com_err(argv[0], retval, "While reading block\n");
 	  o_O ("Error while reading block");
 	  return;
 	}
       for (i = 0; i < current_fs->blocksize; i++)
 	if (buf[i])
 	  break;
-      //      if (i >= current_fs->blocksize){
-      //              printf("\nUnused block %lu contains zero data:\n\n",blk);
-      //              continue;
-      //              }
-
       printf
 	("\nSearching Unused block %lu which contains non-zero data:\n\n",
 	 blk);
 
       if ((blk - last_searched_blk) != 1)
 	{
-	  //some blks are skipped b'cause they are in-use or empty.so reset.
 	  memset ((void *) &needle, '\0', sizeof (struct extcarve_meta));
 	}
 
@@ -239,9 +211,6 @@ do_dump_unused (ext2_filsys current_fs)
 	      //unable to find footer in DIRECT_BLKS blks so reset
 	      memset ((void *) &needle, '\0', sizeof (struct extcarve_meta));
 	    }
-
-	  //for (i=0; i < current_fs->blocksize; i++)
-	  //      fputc(buf[i], stdout);
 	}
       else if (retval == -1)
 	{
@@ -265,7 +234,6 @@ extcarve_write_to_fd (ext2_filsys current_fs, struct extcarve_meta *needle)
   char tmp_dname[100];
   int temfd;
 
-  /* The trick to get a unique name using mkstemp and unlink the temp.file :-)and then use it. */
   temfd = mkstemp (fname);
   close (temfd);
   unlink (fname);
@@ -284,7 +252,6 @@ extcarve_write_to_fd (ext2_filsys current_fs, struct extcarve_meta *needle)
       exit (0);
     }
 
-  //printf("\nRestording files dir : %s %u %u",restore_device_dir,needle->header_blk,needle->footer_blk);
 
   for (blk = needle->header_blk; blk <= needle->footer_blk; blk++)
     {
@@ -305,11 +272,9 @@ int
 extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
 			struct extcarve_meta *needle, unsigned long blk)
 {
-  //read magic file signature and determine file type of given block
-  //gif 
+
   if (buf[0] == 0x47 && buf[1] == 0x49 && buf[2] == 0x46 && buf[3] == 0x38)
     {
-      //printf ("gif header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -321,11 +286,9 @@ extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
       return -1;
     }
 
-  //png 
   if (buf[0] == 0x89 && buf[1] == 0x50 && buf[2] == 0x4E && buf[3] == 0x47
       && buf[4] == 0x0D && buf[5] == 0x0A && buf[6] == 0x1A && buf[7] == 0x0A)
     {
-      //printf ("png header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -336,12 +299,11 @@ extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
       needle->header_found = -1;
       return -1;
     }
-  //jpg -
+
   if ((buf[0] == 0xFF && buf[1] == 0xD8 && buf[2] == 0xFF && buf[3] == 0xE1)
       || (buf[0] == 0xFF && buf[1] == 0xD8 && buf[2] == 0xFF
 	  && buf[3] == 0xE0))
     {
-      //printf ("jpg header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -352,11 +314,9 @@ extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
       needle->header_found = -1;
       return -1;
     }
-  //pdf 
   if ((buf[0] == 0x25 && buf[1] == 0x50 && buf[2] == 0x44 && buf[3] == 0x46
        && buf[4] == 0x2D && buf[5] == 0x31 && buf[6] == 0x2E))
     {
-      //printf ("pdf header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -367,10 +327,8 @@ extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
       needle->header_found = -1;
       return -1;
     }
-  //C/CPP programs
   if (memcmp (buf, "#include", 8) == 0)
     {
-      //printf ("C/C++  header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -381,10 +339,8 @@ extcarve_search4header (unsigned char buf[EXT2_BLOCK_SIZE],
       needle->header_found = -1;
       return -1;
     }
-  //php
   if (memcmp (buf, "<?php>", 6) == 0)
     {
-      //printf ("php  header found!!!");
       if (needle->header_found != 1)
 	{
 	  needle->header_found = 1;
@@ -417,7 +373,6 @@ extcarve_search4footer (unsigned char buf[EXT2_BLOCK_SIZE],
 	  else
 	    return -1;
 	}
-      //png
 
       if (buf[i] == 0x00 && buf[i + 1] == 0x00 && buf[i + 2] == 0x49
 	  && buf[i + 3] == 0x45 && buf[i + 4] == 0x4e && buf[i + 5] == 0x44
@@ -433,8 +388,8 @@ extcarve_search4footer (unsigned char buf[EXT2_BLOCK_SIZE],
 	  else
 	    return -1;
 	}
-      //jpg
-      if (buf[i] == 0xFF && buf[i + 1] == 0xD9)
+	
+	if (buf[i] == 0xFF && buf[i + 1] == 0xD9)
 	{
 	  if (strcmp (needle->dotpart, ".jpg") == 0)
 	    {
@@ -445,7 +400,7 @@ extcarve_search4footer (unsigned char buf[EXT2_BLOCK_SIZE],
 	  else
 	    return -1;
 	}
-      //pdf 
+
       if (buf[i] == 0x25 && buf[i + 1] == 0x25 && buf[i + 2] == 0x45
 	  && buf[i + 3] == 0x4F && buf[i + 4] == 0x46 && buf[i + 5] == 0x0A)
 	{
@@ -458,7 +413,7 @@ extcarve_search4footer (unsigned char buf[EXT2_BLOCK_SIZE],
 	  else
 	    return -1;
 	}
-      //C/C++ 
+
       if ((memcmp (&buf[i], "return", 6) == 0))
 	{
 	  if (strcmp (needle->dotpart, ".cpp") == 0)
@@ -470,7 +425,7 @@ extcarve_search4footer (unsigned char buf[EXT2_BLOCK_SIZE],
 	  else
 	    return -1;
 	}
-      //php 
+
       if ((memcmp (&buf[i], "</php>", 6) == 0))
 	{
 	  if (strcmp (needle->dotpart, ".php") == 0)
